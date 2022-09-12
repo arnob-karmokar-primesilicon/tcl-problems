8261c56d6182650ed0e91a548b5728c952a274ee
Arnob Karmokar <arnob.karmokar@primesilicon.com>
2022-09-12 09:25:00 +0000

    solved problem07

--- beginner/problem07.tcl
+++ beginner/problem07.tcl
@@ -5,16 +5,20 @@ set currentLine 1;
 while {[gets $fp line]>=0} {
 	set words [split $line " "];
 	set count 0;
+	set index 0;
+	set updatedLine $words;
 	foreach word $words {
 		regexp -nocase -indices "summer" $word matched; 
 		if {[info exists matched]==1} {
-			incr count;	
+			incr count;
+			set updatedLine [lreplace $updatedLine $index $index "winter"];
 			unset matched;
 		}
+		incr index;
 	}
 	if {$count>0} {
 		puts "$currentLine: $count";
-		puts $words;
+		puts $updatedLine;
 	}
 	incr currentLine;
 }
