6abb543f1f55e5df78aa44d97fce2ec18150d1f0
Arnob Karmokar <arnob.karmokar@primesilicon.com>
2022-09-12 09:47:31 +0000

    solved problem02

--- beginner/problem02.tcl
+++ beginner/problem02.tcl
@@ -1,11 +1,23 @@
 #! /usr/bin/tclsh
-## unsolved
-proc fibonacci {n} {
-	if {$n<=1} {
-		return $n;
-	} else {
-		return [expr [fibonacci n] + [fibonacci [expr $n - 1]];
-	}
+set t1 0;
+set t2 1;
+
+set nextTerm [expr $t1 + $t2];
+
+puts "Enter a fibonacci series value:";
+gets stdin n;
+
+set START_TIME [clock clicks -microseconds]
+puts -nonewline "The fibonacci series is: $t1 $t2 ";
+
+for {set i 3} {$i<=$n} {incr i} {
+	puts -nonewline "$nextTerm ";
+	set t1 $t2;
+	set t2 $nextTerm;
+	set nextTerm [expr $t1 + $t2];
 }
 
-puts "[fibonacci 10]";
+set END_TIME [clock clicks -microseconds];
+
+puts "";
+puts "Total run time is: [expr $END_TIME - $START_TIME] microseconds";
