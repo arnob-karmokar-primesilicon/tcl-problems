1dbd060af5590c74650bd47880eaf5a5a3c18b61
Arnob (Arnob Karmokar) <arnob.karmokar@primesiliconbd.com>
2022-11-07 13:15:38 +0600

    solved intermediate problem 04

---
+++ intermediate/problem04.tcl
@@ -0,0 +1,60 @@
+proc isPrime {n} {
+	if {$n==0 || $n==1} {
+		return 0
+	}
+	for {set i 2} {$i<=sqrt($n)} {incr i} {
+		if {[expr $n%$i]==0} {
+			return 0
+		}
+	}
+	return 1
+}
+
+proc isComposite {n} {
+	if {$n==0 || $n==1} {
+		return 0
+	}
+	for {set i 2} {$i<=sqrt($n)} {incr i} {
+		if {[expr $n%$i]==0} {
+			return 1
+		}
+	}
+	return 0
+}
+
+puts -nonewline "1. Find composite numbers.\n2. Find prime numbers.\nSelect your operation: ";
+flush stdout
+gets stdin op;
+puts -nonewline "Enter lower range: "
+flush stdout
+gets stdin lowerLimit
+puts -nonewline "Enter higher range: "
+flush stdout
+gets stdin higherLimit
+
+set numList ""
+if {$op==1} {
+	for {set i $lowerLimit} {$i<=$higherLimit} {incr i} {
+		if {[isComposite $i]} {
+			lappend numList $i
+		}
+	}
+	if {[llength $numList]>0} {
+		puts "The list of composite number of the range {$lowerLimit to $higherLimit}: $numList"
+	} else {
+		puts "No composite number in the range {$lowerLimit to $higherLimit}"
+	}
+} elseif {$op==2} {
+	for {set i $lowerLimit} {$i<=$higherLimit} {incr i} {
+		if {[isPrime $i]} {
+			lappend numList $i
+		}
+	}
+	if {[llength $numList]>0} {
+		puts "The list of prime number of the range {$lowerLimit to $higherLimit}: $numList"
+	} else {
+		puts "No prime number in the range {$lowerLimit to $higherLimit}"
+	}
+} else {
+	puts "Invalid Operation!"
+}
