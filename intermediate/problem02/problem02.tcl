efedbf1a21e81cd358735ad0e211dd2220b1a9b9
Arnob (Arnob Karmokar) <arnob.karmokar@primesiliconbd.com>
2022-11-07 17:16:27 +0600

    solved intermediate problem02

---
+++ intermediate/problem02/problem02.tcl
@@ -0,0 +1,19 @@
+set f [open "LIF02_Setup_timing.rpt" r]
+while {[gets $f line]>=0} {
+	if {[regexp "delay250" $line] || [regexp "delay100" $line] || [regexp "DLY" $line]} {
+		set name [string map {"(" "" ")" ""} [lindex $line 1]]		
+		if {[info exists flop($name)]} {
+			set flop($name) [lappend flop($name) [lindex $line 2]] 
+		} else {
+			set flop($name) [lindex $line 2]
+		}
+	}
+}
+close $f
+
+set o [open "output.txt" w]
+foreach item [array names flop] {	
+	puts $o "$item [lindex [lsort $flop($item)] end] #Occurs [llength $flop($item)] times"
+}
+close $o
+exec sort output.txt > output_s.txt
