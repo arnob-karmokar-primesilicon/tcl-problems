aacb6dcbc71d0b507cbfd85f24ecfcd07afd07b8
arnob-karmokar-primesilicon <arnob.karmokar@primesilicon.com>
2022-11-08 18:04:17 +0600

    solved intermediate problem 07

---
+++ intermediate/problem06/problem06.tcl
@@ -0,0 +1,40 @@
+# Author: Arnob Karmokar
+# Email: contact@arnob.me
+# Version: 1.0
+
+set f [open "all_timing_report.rpt" r]
+set o [open "output.csv" w]
+set lineNo 1
+while {[gets $f line]>=0} {
+	if {[regexp {/Y } $line] || [regexp {/ECK } $line] || [regexp {/Q } $line]} {
+		set stdCellName [string map {"(" "" ")" ""} [lindex $line 1]] 
+		if {[info exists stdCell($stdCellName)]} {
+			if {[llength $line]>2} {
+				#puts $o "$lineNo: $stdCellName: [lindex $line 4]"
+				lappend stdCell($stdCellName) [lindex $line 4]
+			} else {
+				gets $f line	
+				lappend stdCell($stdCellName) [lindex $line 2]
+				#puts $o "$lineNo: $stdCellName: [lindex $line 2]"
+			}
+		} else {
+			if {[llength $line]>2} {
+				set stdCell($stdCellName) [lindex $line 4]
+			} else {
+				gets $f line
+				set stdCell($stdCellName) [lindex $line 2]
+			}
+		}
+	}
+	incr lineNo
+}
+#parray stdCell
+puts $o "Libcell,Min_Delay,Avg_Delay,Max_Delay"
+foreach cell [array names stdCell] {
+	set cellDelays [lsort $stdCell($cell)]
+	set totalDelay [expr [join $cellDelays +]]
+	set avgDelay [expr $totalDelay/[llength $cellDelays]]
+	puts $o "$cell,[lindex $cellDelays 0],$avgDelay,[lindex $cellDelays end]"
+}
+
+close $o
