69e584b6792bfe2b8ff486d88ab9a83c676042e0
arnob-karmokar-primesilicon <arnob.karmokar@primesilicon.com>
2022-11-15 13:18:19 +0600

    fixed a logical issue with intermediate problem 02

--- intermediate/problem02/problem02.tcl
+++ intermediate/problem02/problem02.tcl
@@ -1,9 +1,16 @@
 #######################################
 # Title: Intermediate Problem 02 - Extracting Delay Cell Info
 # Author: Arnob Karmokar
-# Version: 1.0
-# Date: 7 Nov 2022
+# Version: 1.1
+# Date: 15 Nov 2022
 #######################################
+
+proc mode {list} {
+    foreach val $list {dict incr h $val}
+    set h [lsort -stride 2 -index 1 -decreasing -integer $h]
+    return [lrange $h 0 1]
+}
+
 set f [open "LIF02_Setup_timing.rpt" r]
 while {[gets $f line]>=0} {
 	if {[regexp "delay250" $line] || [regexp "delay100" $line] || [regexp "DLY" $line]} {
@@ -19,7 +26,7 @@ close $f
 
 set o [open "output.txt" w]
 foreach item [array names flop] {	
-	puts $o "$item [lindex [lsort $flop($item)] end] #Occurs [llength $flop($item)] times"
+	set max_delay [mode $flop($item)]
+	puts $o "$item [lindex $max_delay 0] #Occurs [lindex $max_delay 1] times"
 }
 close $o
-exec sort output.txt > output_s.txt
---
+++ intermediate/problem09/problem09.tcl
@@ -0,0 +1,8 @@
+#######################################
+# Title: Intermediate Problem 09 - Area Extension of list of Rectangular Instance
+# Author: Arnob Karmokar
+# Version: 1.0
+# Date: 15 Nov 2022
+#######################################
+
+
